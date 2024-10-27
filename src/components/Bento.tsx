fde4450b2eba18165b48b967553a8078208b2f07
Julius San Jose <112174347+Joulessies@users.noreply.github.com>
2024-10-27 14:28:10 +0800

    build(src): src file

---
+++ src/components/Bento.tsx
@@ -0,0 +1,23 @@
+import { useState, useId, useRef, useEffect, forwardRef } from 'react';
+
+export type BentoProps = {
+
+};
+
+const Bentos = (props: BentoProps) => {
+    style = {},
+    className = "" || '',
+    bentoWidth = 300,
+    bentoHeight = 300,
+    bentoBackground = 'white',
+    bentoBorder = {},
+    bentoBorderRadius = 0,
+    bentoShadow = {},
+    bentoPadding = {},
+    bentoMargin = {},
+    bentoPosition = {},
+    bentoTop = {},
+    bentoRight = {},
+    bentoBottom = {},
+    bentoLeft = {},
+}
