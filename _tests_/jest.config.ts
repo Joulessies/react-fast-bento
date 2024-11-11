954d9f708e0330efb0a1cbab10217273cf18c501
Julius San Jose <112174347+Joulessies@users.noreply.github.com>
2024-11-11 10:53:18 +0800

    feat(config): jest config update

--- _tests_/jest.config.ts
+++ _tests_/jest.config.ts
@@ -2,6 +2,21 @@ import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'html'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 export default config;
