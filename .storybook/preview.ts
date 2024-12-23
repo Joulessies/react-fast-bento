48efbd205e9891ff99a8ccf6b78b8f5be3235d8f
Julius San Jose <112174347+Joulessies@users.noreply.github.com>
2024-12-23 16:54:50 +0800

    fix(fix): bug
    bug fixeed

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,13 @@ import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      values: [
+        { name: 'Dark', value: '#fff' },
+        { name: 'Light', value: '#000' },
+      ],
+      default: 'Light',
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -3,14 +3,20 @@ import React from 'react';
 interface BentoProps {
   title: string;
   description: string;
+  size: number;
 }
 
-const Bento: React.FC<BentoProps> = ({ title, description }) => (
+const Bento: React.FC<BentoProps> = ({ title, description, size }) => (
   <div
-    style={{ padding: '16px', border: '1px solid #ddd', borderRadius: '8px' }}
+    style={{
+      padding: '16px',
+      border: '1px solid #ddd',
+      borderRadius: '8px',
+      maxWidth: `${size}px`,
+    }}
   >
-    <h1 style={{ margin: 0, fontSize: '24px', color: '#333' }}>{title}</h1>
-    <p style={{ margin: '8px 0 0', fontSize: '16px', color: '#666' }}>
+    <h1 style={{ margin: 0, fontSize: '24px', color: '#fff' }}>{title}</h1>
+    <p style={{ margin: '8px 0 0', fontSize: '16px', color: '#fff' }}>
       {description}
     </p>
   </div>
