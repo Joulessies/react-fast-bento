4e5458fa643ea0662619d60a4cec79c574ceaf46
Julius San Jose <112174347+Joulessies@users.noreply.github.com>
2024-12-29 20:17:21 +0800

    feat(custom): styling
    added styling

--- src/components/BentoGrid/BentoGrid.tsx
+++ src/components/BentoGrid/BentoGrid.tsx
@@ -1,1 +1,15 @@
 import React from 'react';
+
+interface BentoGridProps {
+  columns: number;
+  gap: number;
+  children: React.ReactNode;
+}
+
+export const BentoGrid: React.FC<BentoGridProps> = ({
+  columns,
+  gap,
+  children,
+}) => {
+  return <div className="bentogrid">{children}</div>;
+};
