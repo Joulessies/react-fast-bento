5302121af5c9a82feeaeb4dbecf2baea5e690c9c
Julius San Jose <112174347+Joulessies@users.noreply.github.com>
2025-01-03 15:47:35 +0800

    feat(custom): configuration fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.tsx',
+      name: 'React-Fast-Bento',
+      fileName: format => `react-fast-bento.${format}.tsx`,
+    },
+    rollupOptions: {
+      external: ['react', 'vue'],
+      output: {
+        globals: {
+          react: 'React',
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [react()],
 });
