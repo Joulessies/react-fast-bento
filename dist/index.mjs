cf532bdcd58a5ac5174021b493e81f2356a4a948
Julius San Jose <julius.sanjose08@gmail.com>
2025-02-15 16:43:58 +0800

    Delete dist directory

--- dist/index.mjs
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Bento = ({ columns = 4, spacing = 10, children, style, className = "", }) => {
-    return (React.createElement("div", { className: `grid ${className}`, style: Object.assign({ gridTemplateColumns: `repeat(${columns}, 1fr)`, gap: `${spacing}px` }, style) }, children));
-};
-
-export { Bento };
-//# sourceMappingURL=index.mjs.map
